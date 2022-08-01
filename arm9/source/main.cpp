7f0e984da848464eeda6c746d3fabd3b667552ad
Juan Pablo Neira <juan.neira@aeolabs.io>
2022-08-01 02:21:40 -0400

    add emu env

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -147,6 +147,7 @@ int main(int argc, char **argv) {
 	RenderTGDSLogoMainEngine((uint8*)&TGDSLogoLZSSCompressed[0], TGDSLogoLZSSCompressed_size);
 	
 	/////////////////////////////////////////////////////////Reload TGDS Proj///////////////////////////////////////////////////////////
+	#if !defined(ISEMULATOR)
 	char tmpName[256];
 	char ext[256];	
 	char TGDSProj[256];
@@ -224,6 +225,7 @@ int main(int argc, char **argv) {
 			
 		}
 	}
+	#endif
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	if(__dsimode == true){
 		TWLSetTouchscreenTWLMode();
